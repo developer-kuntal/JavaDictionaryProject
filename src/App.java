57b7f04b786c02b4bd72251b71b4be039c612025
kuntal-designs-codes <kuntalkantidas8@gmail.com>
2021-08-02 19:59:22 +0530

    commit1

--- src/App.java
+++ src/App.java
@@ -1,34 +1,72 @@
 // import org.apache.log4j.Logger;  
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-import java.util.ArrayList;
+import java.net.URLEncoder;
+import java.io.UnsupportedEncodingException;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
+
+    public static String uri = "mongodb://localhost:27017";
+    public static final MongoClient mongoClient = MongoClients.create(uri);
+    public static final MongoDatabase database = mongoClient.getDatabase("testdb");
+    public static final MongoCollection<Document> collection = database.getCollection("words");
+    public static final List<Document> documents = new ArrayList<Document>();
 
     public static void readLines(File f) throws IOException {
         FileReader fr = new FileReader(f);
         BufferedReader br = new BufferedReader(fr);
         String line;
+        String baseUrl = "https://www.google.com/search?q=";
+        String completeUrl = null;
+
         while((line = br.readLine()) != null) {
+
             // System.out.println(line);
-            WebScrapping web = new WebScrapping("https://www.google.com/search?q="+line+"meaning+in+bengali");
+            try {
+                String queryString = line+"meaning+in+bengali";
+                String encodedQuery = URLEncoder.encode(queryString, "UTF-8");
+                completeUrl = baseUrl + encodedQuery;
+            } catch (UnsupportedEncodingException e) {
+                //TODO: handle exception
+                System.out.println("Issue while decoding" +e.getMessage());
+            }
+
+            WebScrapping web = new WebScrapping(completeUrl);
+
             try {
                 ArrayList<String> bnList = new ArrayList<>();
                 bnList = web.WebScrappingData();
                 // System.out.println("List of Word: "+bnList);
-                MongoOperation mOperation = new MongoOperation(line, bnList);
-                mOperation.saveDocument();
+                Document doc = new Document();
 
+                doc.put("word", line);
+                doc.put("bengali_meaning", bnList);
+
+                documents.add(doc);
+                // MongoOperation mOperation = new MongoOperation(line, bnList);
+                // mOperation.saveDocument();
+                // collection.insertOne(doc);
+
+                System.out.println(doc.toJson());
             } catch (IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
+        collection.insertMany(documents);
         System.out.println("Done!!!");
         br.close();
         fr.close();
@@ -36,7 +74,7 @@ public class App {
 
     public static void main( String[] args ) {
         
-        File f = new File("D:\\JavaProject\\DictionaryProject\\data\\dictionary.txt");
+        File f = new File("D:\\JavaProject\\DictionaryProject\\data\\xab");
         try {
             readLines(f);
         } catch (Exception e) {
--- src/WebScrapping.java
+++ src/WebScrapping.java
@@ -1,28 +1,78 @@
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 import java.io.IOException;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 
 public class WebScrapping {
 
     public String url;
+    public ArrayList<String> listofWord = new ArrayList<>();
+    public Document doc = null;
+    public int i = 0;
+    public boolean success = false;
 
-    public WebScrapping(String url) { 
+    public Connection session = Jsoup.newSession()
+                            .timeout(60 * 1000)
+                            .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36");
+
+    public WebScrapping(String url) {
         this.url = url;
     }
 
     public ArrayList<String> WebScrappingData() throws IOException {
-        try {
-            // fetch the document over HTTP
-            Document doc = Jsoup.connect(url).get();
+        while( i < 3) {
+            try {
+                // System.out.println("Entering...."+i);
+                // fetch the document over HTTP
+                // doc = Jsoup.connect(url).
+                    // header("Accept-Encoding", "gzip, deflate").
+                    // header("Content-Type", "text/html, application/xhtml+xml, application/xml;q=0.9, image/webp, */*;q=0.8").
+                    // userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36").
+                    // timeout(80000).followRedirects(true).get();
+
+                doc = session.newRequest()
+                        .url(url)
+                        .get();
+                
+                if( doc.select(".hrcAhc").isEmpty() == true ) {
+                    System.out.println("Not Found"+i);
+                }
+
+                if( doc.select(".hrcAhc").isEmpty() == false ) {  
+                    success = true;
+                    break;
+                }
+                // get the page title
+                // String title = doc.title();
+                // System.out.println("title: " + title);
+                
+                // get all links in page
+                // Elements bnList = doc.select(".hrcAhc");
+                // for (Element list : bnList) {
+                //     // get the value from the href attribute
+                //     listofWord.add(list.text());
+                //     // System.out.println("List of Word: " + list.text());
+                // }
+                // return listofWord;
+
+            } catch(SocketTimeoutException e) {
+                System.out.println("Timeout: URL"+url);
+            }
+            catch (IOException e) {
+                System.out.println("IOException"+url);
+                e.printStackTrace();
+            }
+            i++;
             
-            // get the page title
-            // String title = doc.title();
-            // System.out.println("title: " + title);
-            ArrayList<String> listofWord = new ArrayList<>();
-            // get all links in page
+        }
+
+        if(success) {
+            // Selector code ...
             Elements bnList = doc.select(".hrcAhc");
             for (Element list : bnList) {
                 // get the value from the href attribute
@@ -30,11 +80,12 @@ public class WebScrapping {
                 // System.out.println("List of Word: " + list.text());
             }
             return listofWord;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+        } else {
+            listofWord.add("TimeOut/Not Found");
+            System.out.println("TimeOut");
+            return listofWord;
         }
+        
     }
 
 }
